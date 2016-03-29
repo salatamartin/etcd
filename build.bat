76cede8975d7a958824cfb2d616eb0cd7b32fbc6
Martin Šalata <Martin alata>
2016-03-29 18:51:35 +0200

    win buildfile modified (environment variables)

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@
 SET ORG_PATH=github.com\coreos
 SET REPO_PATH=%ORG_PATH%\etcd
 
+SET MAINGOPATH=%GOPATH%
+
 SET GOPATH=%cd%\gopath
 
 :: Cleanup old builds
@@ -18,8 +20,10 @@ FOR /f "usebackq tokens=*" %%a IN (`go env`) DO %%a
 (FOR /f "tokens=*" %%i IN ('git rev-parse --short HEAD') DO SET GIT_SHA=%%i) 2>NUL
 IF NOT DEFINED GIT_SHA SET GIT_SHA=GitNotFound
 
+SET GOPATH=%MAINGOPATH%
+
 :: Static compilation is useful when etcd is run in a container
 SET CGO_ENABLED=0
-go build -a -installsuffix cgo -ldflags "-s -X %REPO_PATH%\version.GitSHA %GIT_SHA%" -o bin\etcd.exe "%REPO_PATH%"
+go build -a -installsuffix cgo -ldflags "-s -X %REPO_PATH%\version.GitSHA=%GIT_SHA%" -o bin\etcd.exe "%REPO_PATH%"
 :: TODO: Get the %GIT_SHA% argument to work. Useful for `etcd --version` style commands.
 go build -a -installsuffix cgo -ldflags "-s" -o bin\etcdctl.exe "%REPO_PATH%\etcdctl"
